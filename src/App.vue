e903e9f9710dadf94de71bf96862010a4939313b
用户名 <hujuan@kaishengit.com>
2022-04-22 10:05:01 +0800

    添加组件

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">点击</button>
+  <Modal></Modal>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "Hello World",
+    };
+  },
+  methods: {
+    handleClick() {
+      //获得控件名称
+      console.log(this.$refs.name);
+      //添加类
+      this.$refs.name.classList.add("active");
+      //获得焦点
+      this.$refs.name.focus();
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="backrop">
+    <div class="modal">
+      <P>你好</P>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+.backrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+}
+</style>
